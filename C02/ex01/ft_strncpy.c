fdc86d4eef7651980a187ad6ace1019d186b1ad5
DebPeekers <96426109+DebPeekers@users.noreply.github.com>
2022-03-01 10:13:41 +1030

    Add files via upload

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpickard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 11:28:49 by dpickard          #+#    #+#             */
+/*   Updated: 2022/01/25 16:49:25 by dpickard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<string.h>
+#include<stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
