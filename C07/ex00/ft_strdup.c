a4e4f42714ce5b6f23483a1618116c27ece0923f
DebPeekers <96426109+DebPeekers@users.noreply.github.com>
2022-03-01 10:16:27 +1030

    Add files via upload

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpickard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 20:50:26 by dpickard          #+#    #+#             */
+/*   Updated: 2022/02/02 12:40:11 by dpickard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		i;
+
+	copy = malloc(sizeof(*src));
+	if (copy == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy [i] = '\0';
+	return (copy);
+}
+/*
+int	main(void)
+{
+	printf("%s\n", ft_strdup("s"));
+	return (0);
+}
+*/
